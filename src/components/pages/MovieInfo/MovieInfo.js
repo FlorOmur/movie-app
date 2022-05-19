f0b72f3a06cc1fa7055cb776fcc9699abaeac2b4
Flor Omurbekov <flor.omurbekov@mail.ru>
2022-05-19 16:33:06 +0300

    new-components

--- src/components/pages/MovieInfo/MovieInfo.js
+++ src/components/pages/MovieInfo/MovieInfo.js
@@ -23,7 +23,7 @@ const MovieInfo = () => {
         setCredits(res.data)
         setCreditLoader(false)
       })
-  }, [])
+  }, [id])
 
   if (filmLoader || creditLoader) {
     return 'Loading.....'
